790dfeceb8a64ec039d3e57426e0e71555fac9d4
StephenLegacy <oloostephen20191@gmail.com>
2025-06-17 12:40:00 +0300

    check in sync leo

--- ideal.sh
+++ ideal.sh
@@ -2,7 +2,7 @@
 
 # Enhanced script to backdate Git commits with better error handling and flexibility
 
-# Set the backdate timestamp (default: 2025-06-30 11:32:00)
+# Set the backdate timestamp (default: 2025-06-25 11:32:00)
 BACKDATE="${1:-"2025-06-17T12:40:00"}"
 
 
